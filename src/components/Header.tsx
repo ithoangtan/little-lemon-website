9b5a245097805fdf13b9b24aa4ed775cf6a2c588
Hoàng Tấn <tanht2@vng.com.vn>
2023-09-01 15:32:28 +0700

    chore (SEO): add og meta tags

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+function Header() {
+	return (
+		<header>
+			<img src="path_to_logo.png" alt="Little Lemon Logo" />
+		</header>
+	);
+}
+export default Header;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,14 @@
+function Nav() {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<a href="/">Home</a>
+				</li>
+				{/* Other links */}
+			</ul>
+		</nav>
+	);
+}
+
+export default Nav;
