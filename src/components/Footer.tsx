9b32b7c06277ec7c32722eff941c2603538cf002
Hoàng Tấn <tanht2@vng.com.vn>
2023-09-02 13:34:51 +0700

    feat (comp): add reservation

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-	return (
-		<footer>
-			<p>&copy; 2023 Little Lemon</p>
-		</footer>
-	);
-}
-
-export default Footer;
--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-function Header() {
-	return (
-		<header>
-			<img src="path_to_logo.png" alt="Little Lemon Logo" />
-		</header>
-	);
-}
-export default Header;
